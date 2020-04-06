4a8c1a0a9c5523bff6ae8d504e94d399e713a699
Pruthvi Perumalla <pruthviperumalla@gmail.com>
2020-04-05 23:14:56 -0400

    Adding a common file for feature extraction

---
+++ feature_extractor.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+def add_location_features(train_df, events_df, users_df):
+    events_df = events_df[['event_id', 'city', 'country']]
+    merged_df = pd.merge(train_df, events_df, how='inner', left_on='event', right_on='event_id', suffixes=('', '_event'))
+    return merged_df
