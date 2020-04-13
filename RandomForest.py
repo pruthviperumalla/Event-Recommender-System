76ac5c43484b10e3f1bcc823633df9031699ac1e
ramyasree9 <ramyasusanth@gmail.com>
2020-04-12 21:14:34 -0400

    f0.5 metric

--- RandomForest.py
+++ RandomForest.py
@@ -1,10 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score 
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import fbeta_score, make_scorer
 
 def fit(X_train, y_train):
-    model = RandomForestClassifier(n_estimators = 10, max_depth = 10)
+    model = RandomForestClassifier(n_estimators = 150, max_depth = 12)
     # model.fit(X_train, y_train)
-    scores = cross_val_score(model, X_train, y_train, cv=5)
+    metric = make_scorer(fbeta_score, beta=2)
+    scores = cross_val_score(model, X_train, y_train, cv=5, scoring = metric)
     print(scores)
     return model
 
