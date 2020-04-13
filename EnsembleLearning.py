a3348bb2f148602c470c6a8a07f018bce877cb2e
ramyasree9 <ramyasusanth@gmail.com>
2020-04-13 18:24:55 -0400

    split update, ensemble model

---
+++ EnsembleLearning.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import fbeta_score, make_scorer
+
+def fit(X_train, y_train, estimators):
+    # estimators=[(‘knn’, knn_best), (‘rf’, rf_best), (‘log_reg’, log_reg)]
+    ensemble_model = VotingClassifier(estimators, voting='hard')
+    metric = make_scorer(fbeta_score, beta=0.5)
+    scores = cross_val_score(ensemble_model, X_train, y_train, cv=5, scoring = metric)
+    print(scores)
+    return ensemble_model
+
+def fit_predict(X_train, y_train, X_test, ensemble_model):
+    ensemble_model.fit(X_train, y_train)
+    return ensemble_model.predict(X_test)
--- KNN.py
+++ KNN.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import fbeta_score, make_scorer
 
 def fit(X_train, y_train):
-    model = KNeighborsClassifier(n_neighbors = 30, weights = 'distance', algorithm = 'auto')
+    model = KNeighborsClassifier(n_neighbors = 15, weights = 'distance', algorithm = 'auto')
     # model.fit(X_train, y_train)
     metric = make_scorer(fbeta_score, beta=0.5)
     scores = cross_val_score(model, X_train, y_train, cv=5, scoring = metric)
