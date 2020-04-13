60a7a1cb4ce1f820b04117b2f0f1f59e512bf095
ramyasree9 <ramyasusanth@gmail.com>
2020-04-12 22:24:05 -0400

    KNN

--- KNN.py
+++ KNN.py
@@ -1,9 +1,9 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import fbeta_score, make_scorer
 
 def fit(X_train, y_train):
-    model = RandomForestClassifier(n_estimators = 150, max_depth = 12)
+    model = KNeighborsClassifier(n_neighbors = 10, weights = 'distance', algorithm = 'auto')
     # model.fit(X_train, y_train)
     metric = make_scorer(fbeta_score, beta=0.5)
     scores = cross_val_score(model, X_train, y_train, cv=5, scoring = metric)
